bc19467a3905e5b9235a64a626308d7e2dced51b
Andre Wiggins <andwi@microsoft.com>
2017-07-23 22:04:57 -0700

    Add jsdoc comments

--- picodom.d.ts
+++ picodom.d.ts
@@ -12,12 +12,25 @@ export interface Component<Data> {
 
 export type VElement<Data> = VirtualNode<Data> | string
 
+/**
+ * A function to create picodom virtual nodes
+ * @param tag The Component or string of the virtual node to create
+ * @param data The data to store in the virutal node
+ * @param children The children of the virtual node
+ */
 export function h<Data>(
   tag: Component<Data> | string,
   data?: Data,
   children?: Array<VElement<{}> | number>
 ): VirtualNode<Data>
 
+/**
+ * @deprecated Use the children array version of h for better performance. 
+ * This overload primarly exists for JSX support only.
+ * @param tag The Component or string of the virtual node to create
+ * @param data The data to store in the virutal node
+ * @param children The children of the virtual node
+ */
 export function h<Data>(
   tag: Component<Data> | string,
   data?: Data,
