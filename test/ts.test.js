9b0da20b65637894a6c08378b51a83c76b33c31d
Andre Wiggins <andwi@microsoft.com>
2017-11-02 04:02:52 -0700

    Use built-in typescript diagnostic formatting

--- test/ts.test.js
+++ test/ts.test.js
@@ -11,25 +11,31 @@ const expectedErrors = {
     7: 2345,
     11: 2345,
     16: 2322,
-    17: 2322,
+    17: 2322
   }
 }
 
-const tsOptions = loadConfigOptions(path.join(__dirname, "tsconfig.json"))
+const tsOptions = loadConfigOptions(
+  path.join(__dirname, "./typings/tsconfig.json")
+)
 
 // Should there be assertions per thing I want to test? Investigate using transpileModule to compile individual strings
 
-test("typescript definitions compile correctly", () => {
-})
+test("typescript definitions compile correctly", () => {})
 
-test("typescript definitions throw expected errors", () => {
-})
-
+test("typescript definitions throw expected errors", () => {})
 
 function loadConfigOptions(tsConfigPath) {
+  const formattingHost = {
+    getCurrentDirectory: () => process.cwd(),
+    getNewLine: () => "\n",
+    getCanonicalFileName: name => path.relative(process.cwd(), name)
+  }
+
   const tsConfig = ts.readConfigFile(tsConfigPath, ts.sys.readFile)
   if (tsConfig.error) {
-    throw getError(tsConfig.error)
+    let errorText = ts.formatDiagnostics([tsConfig.error], formattingHost)
+    throw "Error parsing tsconfig.json.\n" + errorText
   }
 
   const result = ts.parseJsonConfigFileContent(
@@ -40,57 +46,10 @@ function loadConfigOptions(tsConfigPath) {
     path.basename(tsConfigPath)
   )
 
-  for (const info of result.errors) {
-    console.error(getError(info).message)
-  }
-
-  if (tsConfig.error || result.errors.length > 0) {
-    throw new Error("Error parsing tsconfig.json. See log above for details")
+  if (result.errors.length) {
+    let errorText = ts.formatDiagnostics(result.errors, formattingHost)
+    throw "Error parsing tsconfig.json.\n" + errorText
   }
 
   return result.options
-}
-
-function getError(info) {
-  const err = new Error()
-
-  let filePreamble = ""
-  if (info.file && info.start) {
-    const startPos = ts.getLineAndCharacterOfPosition(info.file, info.start)
-
-    filePreamble =
-      path.relative(process.cwd(), info.file.fileName) +
-      `(${startPos.line + 1},${startPos.character + 1}): `
-
-    const endPos = ts.getLineAndCharacterOfPosition(
-      info.file,
-      info.start + info.length
-    )
-
-    err.startPosition = {
-      position: info.start,
-      line: startPos.line,
-      character: startPos.character
-    }
-
-    err.endPosition = {
-      position: info.start + info.length - 1,
-      line: endPos.line,
-      character: endPos.character
-    }
-  }
-
-  const codeAndMessageText =
-    filePreamble +
-    ts.DiagnosticCategory[info.category].toLowerCase() +
-    " TS" +
-    info.code +
-    ": " +
-    ts.flattenDiagnosticMessageText(info.messageText, "\n")
-
-  err.name = "TypeScript Error"
-  err.message = codeAndMessageText
-  err.diagnostic = info
-
-  return err
 }
